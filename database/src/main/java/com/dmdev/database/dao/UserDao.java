8429a33dacf194ffbe7b779d523a71e69277906d
user <exchange.program2018@gmail.com>
2023-01-16 19:00:48 +0300

    my custom plugin

--- database/src/main/java/com/dmdev/database/dao/UserDao.java
+++ database/src/main/java/com/dmdev/database/dao/UserDao.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 
 public class UserDao {
 
-    public Optional<User> findById(Long id) {
+    public Optional<User > findById(Long id) {
         return Optional.empty();
 
     }
